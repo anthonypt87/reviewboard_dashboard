40bc6f5e57c81ea06ed4aade98bb4b5c2e7e2782
anthony <anthonypt87@gmail.com>
2013-06-09 16:36:49 -0700

    RBClient review requests are now wrapped and returned by ReviewboardClient.

--- reviewboard_client.py
+++ reviewboard_client.py
@@ -1,5 +1,6 @@
 """Thin wrapper around ``rbtools.api.client.RBClient```"""
 
+
 class ReviewboardClient(object):
 	def __init__(self, rb_client):
 		self._rb_client = rb_client
@@ -13,4 +14,49 @@ class ReviewboardClient(object):
 			review_request_list_resource = review_request_list_resource.get_next(**filters)
 
 
+class ReviewRequest(object):
+	def __init__(self, id, submitter, reviewers, summary, description, reviews):
+		self.id = id
+		self.submitter = submitter
+		self.reviewers = reviewers
+		self.description = description
+		self.summary = summary
+		self.reviews = reviews
 
+	def __repr__(self):
+		return 'ReviewRequest(id=%s, submitter=%s, reviewers=%s, summary=%s, description=%s, reviews=%s)' % (
+			self.id,
+			self.submitter,
+			self.reviewers,
+			self.summary,
+			self.description,
+			self.reviews,
+		)
+
+	@classmethod
+	def create_from_rb_client_review_request(cls, rb_client_review_request):
+		submitter = rb_client_review_request.get_submitter().fields['username']
+
+		fields = rb_client_review_request.fields
+		id = fields['id']
+		reviewers = [reviewer['title'] for reviewer in fields['target_people']]
+		description = fields['description']
+		summary = fields['summary']
+
+		reviews = []
+		rb_reviews = rb_client_review_request.get_reviews()
+		for rb_review in rb_reviews:
+			reviewer = rb_review.get_user().fields['username']
+			reviews.append({
+				'ship_it': rb_review.fields['ship_it'],
+				'reviewer': reviewer
+			})
+
+		return cls(
+			id,
+			submitter,
+			reviewers,
+			summary,
+			description,
+			reviews,
+		)
--- tests/reviewboard_client_test.py
+++ tests/reviewboard_client_test.py
@@ -2,6 +2,8 @@ import unittest
 
 import mock
 from reviewboard_client import ReviewboardClient
+from reviewboard_client import ReviewRequest
+
 
 class MockRBClient(object):
 
@@ -32,22 +34,78 @@ class MockRBClient(object):
 	def _create_mock_review_resource(self):
 		return mock.Mock(
 			fields={
-				'status': 'pending',
-				'id': 3
+				'status': 'pending', 'id': 3
 			}
 		)
 
 
 class ReviewboardClientTest(unittest.TestCase):
 
-	def setUp(self):
-		self._mock_rb_client_patcher = mock.patch('reviewboard_client.RBClient', MockRBClient)
-		self._mock_rb_client = self._mock_rb_client_patcher.start()()
-
-	def tearDown(self):
-		self._mock_rb_client_patcher.stop()
-
-	def test_get_review_request(self):
-		reviewboard_client = ReviewboardClient(self._mock_rb_client)
+	def test_get_review_requests(self):
+		reviewboard_client = ReviewboardClient(MockRBClient())
 		reviews = list(reviewboard_client.get_review_requests())
 		self.assertEqual(len(reviews), 2)
+
+
+class ReviewRequestTest(unittest.TestCase):
+
+	def test_create_from_rb_client_review_request(self):
+		rb_client_review_request = mock.Mock(
+			get_submitter=lambda: mock.Mock(
+				fields={
+					'username': 'anthony'
+				}
+			),
+			fields={
+				u'branch': u'anthony/anthony_is_the_best',
+				u'bugs_closed': [u'47909'],
+				u'changenum': None,
+				u'description': u'Primary reviewer: andy\nAnthony is the best',
+				u'id': 32868,
+				u'last_updated': u'2013-06-09 14:10:46',
+				u'public': True,
+				u'status': u'pending',
+				u'summary': u'Proving once and for all he is the greatest',
+				u'target_groups': [
+					{
+						u'href': u'https://reviewboard.anthony.com/api/groups/group1/',
+						u'method': u'GET',
+						u'title': u'group1'
+					}
+				],
+				u'target_people': [
+					{
+						u'href': u'https://reviewboard.anthony.com/api/users/andy/',
+						u'method': u'GET',
+						u'title': u'andy'
+					}
+				],
+				u'testing_done': u'Wrote some tests',
+				u'time_added': u'2013-06-06 09:50:57'
+			},
+			get_reviews=lambda: [
+				mock.Mock(
+					fields={
+						u'ship_it': True,
+					},
+					get_user=lambda: mock.Mock(
+						fields={
+							'username': 'andy'
+						}
+					)
+				)
+			]
+		)
+		review_request = ReviewRequest.create_from_rb_client_review_request(rb_client_review_request)
+		self.assertEqual(review_request.id, 32868)
+		self.assertEqual(review_request.submitter, 'anthony')
+		self.assertEqual(review_request.reviewers, ['andy'])
+		self.assertEqual(review_request.summary, rb_client_review_request.fields['summary'])
+		self.assertEqual(review_request.description, rb_client_review_request.fields['description'])
+		self.assertEqual(
+			review_request.reviews,
+			[{
+				'ship_it': True,
+				'reviewer': 'andy'
+			}]
+		)
